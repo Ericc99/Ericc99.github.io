8daa3e92d0bf2132b6355a5d221069533af26b3b
Ericc99 <ericc99@foxmail.com>
2024-10-18 00:57:34 +0800

    Modify paper date

--- src/components/Home/PublicationCards.js
+++ src/components/Home/PublicationCards.js
@@ -9,6 +9,26 @@ function PublicationCards(props) {
   return (
     <Container>
         <h1 style={{ fontSize: "1.4em" }}>
+            <span className="black"> <strong>2024</strong></span>
+        </h1>
+        <br/>
+        <Row>
+            <Col md={3} className="publication-block">
+            <img
+                src={Paper2}
+                alt="profile Pic"
+                className="img-fluid"
+                style={{ width: "240px", height: "120px", borderRadius: 0 }}
+            />
+            </Col>
+            <Col md={9}>
+                <p className="publication-link" >A Modelling and Optimization Methodology for High-Throughput Synthesis Automation Systems</p>
+                <p className="publication-authors">Co-First Author</p>
+                <p className="publication-conference">Under preparations for IEEE Robotics and Automation Letters (RA-L)</p>
+            </Col>
+        </Row>
+        <br/>
+        <h1 style={{ fontSize: "1.4em" }}>
             <span className="black"> <strong>2022</strong></span>
         </h1>
         <br/>
@@ -27,22 +47,6 @@ function PublicationCards(props) {
                 <p className="publication-conference">Chemical Engineering Science</p>
                 <FaRegFilePdf className="pub-icon" /> &nbsp;
                 {<a className="pub-link" href="https://www.sciencedirect.com/science/article/pii/S0009250922005802" style={{ textDecoration: "none" }} target="_blank" rel="noreferrer">PDF</a>}
-            </Col>
-        </Row>
-        <br/>
-        <Row>
-            <Col md={3} className="publication-block">
-            <img
-                src={Paper2}
-                alt="profile Pic"
-                className="img-fluid"
-                style={{ width: "240px", height: "120px", borderRadius: 0 }}
-            />
-            </Col>
-            <Col md={9}>
-                <p className="publication-link" >A Modelling and Optimization Methodology for High-Throughput Synthesis Automation Systems</p>
-                <p className="publication-authors">Co-First Author</p>
-                <p className="publication-conference">Under preparations for IEEE Robotics and Automation Letters (RA-L)</p>
             </Col>
         </Row>
     </Container>
